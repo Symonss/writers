ce70c2e0da7505e0b60619cdc115b2c4ee46b267
rngode <raquelngode@gmail.com>
2019-01-31 13:32:32 +0300

    added several classes and relation

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Order
 from .models import Comment
+from .models import Status
+from .models import Bid
 # we register our objects here(these are the tables)
 admin.site.register(Order)
 admin.site.register(Comment)
+admin.site.register(Status)
+admin.site.register(Bid)
+
--- coreapp/models.py
+++ coreapp/models.py
@@ -6,24 +6,41 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Order's table
-class Order(models.Model):
-    client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    order_topic = models.CharField(max_length=200, blank=True)
-    order_description = models.TextField(blank=True)
-    track_number = models.IntegerField()
-    is_paid = models.BooleanField(default=False)
-    date_created = models.DateTimeField()
-    deadline = models.DateTimeField()
+
+class Status(models.Model):
     is_available = models.BooleanField(default=False)
     is_pending = models.BooleanField(default=False)
     is_approved = models.BooleanField(default=False)
     is_editing = models.BooleanField(default = False)
+    is_paid=models.BooleanField(default=False)
+    is_rejected=models.BooleanField(default=False)
+
+# Order's table
+class Order(models.Model):
+    client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+   # writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    order_topic = models.CharField(max_length=200, blank=True)
+    order_description = models.TextField(blank=True)
+    track_number = models.IntegerField()
+    status=models.ForeignKey(Status, default=1, on_delete=models.CASCADE)
+    # is_paid = models.BooleanField(default=False)
+    date_created = models.DateTimeField()
+    deadline = models.DateTimeField()
+    # is_available = models.BooleanField(default=False)
+    # is_pending = models.BooleanField(default=False)
+    # is_approved = models.BooleanField(default=False)
+    # is_editing = models.BooleanField(default = False)
     order_files = models.CharField(max_length=200, blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    def assignorder(self):
+        status= status.is_available(False)
+        self.save()
+
 
     def __str__(self):
         return self.order_topic
@@ -33,3 +50,14 @@ class Comment(models.Model):
     user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time=models.DateTimeField()
     is_seen=models.BooleanField(default=False)
+
+
+class Bid(models.Model):
+    order=models.ForeignKey(Order, on_delete=models.CASCADE)
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bid_amount=models.IntegerField()
+
+
+
+ 
+      
